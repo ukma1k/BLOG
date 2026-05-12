02ee9047d5901ba7cdd6faeca0441be6020868ac
ukma1k <boculakmarta183@gmail.com>
2026-05-12 20:38:36 +0300

    Blog

--- Blog/urls.py
+++ Blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Blog_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",views.post_list, name="post_list"),
 ]
